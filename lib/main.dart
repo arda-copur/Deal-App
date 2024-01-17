0d4530beef76a3a459be03ca092baa6adecb413b
arda-copur <ardacopur0@gmail.com>
2024-01-17 23:41:00 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-    
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.pink)
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
