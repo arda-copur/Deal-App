dbda10c3638da39ae6d90974bd9e3b8b834a4c69
arda-copur <ardacopur0@gmail.com>
2024-01-18 00:33:48 +0300

    app colors

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFFF2F08);
+  static const Color secondaryColor = Color(0xFF0AE91F);
+  static const Color thirdColor = Color(0xFF54F063);
+  static const Color exColor = Color(0xFF9BF6A4);
+  static const Color basicBlack = Colors.black;
+  static const Color basicWhite = Colors.white;
+  static const Color basicRed = Colors.red;
+  static const Color basicGrey = Colors.grey;
+  static const Color basicTransparent = Colors.transparent;
+  
+}
