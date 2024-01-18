76239bb32b42bfa5dd796bde2692091d8bfb0dcb
arda-copur <ardacopur0@gmail.com>
2024-01-18 14:08:53 +0300

    constants

---
+++ lib/constants/app_texts.dart
@@ -0,0 +1,19 @@
+class AppTexts {
+  // Home View
+  static String homeSlogan = "İstediğin her şey burada!";
+  static String homeCity = "İstanbul";
+  static String homeSearchText = "İstediğin ürünü ara...";
+  static String homeCategories = "Kategoriler";
+  static String homeSeeAll = "Hepsini gör";
+  static String homePopular = "Popüler Ürünler";
+  static String homeWarranty = "2 Yıl Garantili";
+  static String homeLike = "Beğen";
+  static String homePrice = "749 TL";
+  // Bottom View
+  static String labelHome = "Ana Sayfa";
+  static String labelAdv = "Ürünler";
+  static String labelShare = "Ekle";
+  //Advertisements Views
+  static String notYet = "Henüz ilan paylaşılmamış.";
+  static String description = "Açıklama";
+}
